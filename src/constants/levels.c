a8e048271b707cebc71e77de7d81b3dc8024f27c
v81d <v81d@proton.me>
2026-07-25 17:06:58 -0400

    feat: create levels list with new structure

---
+++ src/constants/levels.c
@@ -0,0 +1,18 @@
+#include "constants/levels.h"
+
+/** The list of levels in the game. */
+const struct Level levels[3] = {{
+                                    .id = 1,
+                                    .name = "Endless",
+                                    .difficulty = 1,
+                                },
+                                {
+                                    .id = 2,
+                                    .name = "Stardust",
+                                    .difficulty = 2,
+                                },
+                                {
+                                    .id = 3,
+                                    .name = "Inertia",
+                                    .difficulty = 3,
+                                }};
---
+++ src/constants/levels.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdint.h>
+
+/** A structure representing a single level/map. */
+struct Level {
+    uint8_t id;         ///< The level ID/number.
+    char name[8];       ///< The name of the level (maximum: 8 characters).
+    uint8_t difficulty; ///< The difficulty of the level (1-5).
+};
