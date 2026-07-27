a505e908bd84ec00e0d23cc59931bc04cfc6c9a6
Nom075 <graytonl@nycstudents.net>
2026-07-27 06:53:25 -0700

    docs: update levels.h documentation

--- src/constants/levels.h
+++ src/constants/levels.h
@@ -10,9 +10,8 @@ struct Level {
     uint8_t id;         ///< The level ID/number.
     char name[11];      ///< The name of the level (maximum: 10 characters).
     uint8_t difficulty; ///< The difficulty of the level (1-5).
-    // name and difficulty has +1 than what is needed for \0 room.
 };
 
 /** The list of levels in the game. */
 extern const struct Level
-    levels[LEVEL_COUNT]; ///< Get number of levels using levels_length.
+    levels[LEVEL_COUNT];
