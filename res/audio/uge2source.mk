5f518bd50b5d79e38d312ca1fbb7fd7fed25b6f6
Nom075 <graytonl@nycstudents.net>
2026-07-27 09:11:36 -0700

    feat(soundtrack): add Isolation

--- res/audio/uge2source.mk
+++ res/audio/uge2source.mk
@@ -1,6 +1,6 @@
 # List of assets to include and convert.
 # Paths should be relative to res/audio/.
-# Please keep this list sorted in alphabetical order.
 UGE2SOURCE_ASSETS := \
     soundtrack/menu/menu_loop.uge \
-    soundtrack/levels/01_resurrect/resurrect.uge
+    soundtrack/levels/01_resurrect/resurrect.uge \
+    soundtrack/levels/05_isolation/isolation.uge
--- src/main.c
+++ src/main.c
@@ -10,7 +10,7 @@
  *
  * From /res/audio/soundtrack/menu/menu_loop.uge
  */
-extern const hUGESong_t menu_loop;
+extern const hUGESong_t isolation;
 
 void main(void) {
     uint8_t current_sprite = 0;
@@ -20,7 +20,7 @@ void main(void) {
     NR51_REG = 0xFF; // enable sound
                      //
     CRITICAL {
-        hUGE_init(&menu_loop);
+        hUGE_init(&isolation);
         add_VBL(hUGE_dosound);
     }
 
