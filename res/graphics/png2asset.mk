b1a297bdad00351509b94ea096ba5cea731e3d0a
Nom075 <graytonl@nycstudents.net>
2026-07-29 09:38:58 -0700

    feat(assets): add black filler

--- res/graphics/png2asset.mk
+++ res/graphics/png2asset.mk
@@ -4,6 +4,7 @@
 PNG2ASSET_ASSETS := \
     backgrounds/level_background \
     backgrounds/menu_background \
+    fillers/black_filler \
     fonts/orochi_jp_16x16 \
     fonts/yarara_font_8x8 \
     notes/a_reverse_note \
@@ -16,6 +17,7 @@ PNG2ASSET_ASSETS := \
 # Please keep this list sorted in alphabetical order.
 PNG2ASSET_ASSET_FLAGS/backgrounds/level_background := -map -use_map_attributes
 PNG2ASSET_FLAGS/backgrounds/menu_background := -map -use_map_attributes
+PNG2ASSET_FLAGS/fillers/black_filler := -spr8x8 -sw 8 -sh 8
 PNG2ASSET_FLAGS/fonts/orochi_jp_16x16 := -spr8x16 -sw 16 -sh 16
 PNG2ASSET_FLAGS/fonts/yarara_font_8x8 := -spr8x8 -sw 8 -sh 8 -sprite_no_optimize
 PNG2ASSET_ASSET_FLAGS/notes/a_reverse_note := -spr16x16 -sw 16 -sh 16
