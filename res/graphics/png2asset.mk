36fd55bb3a33a29ddb8dd8cbcc098b4299eb2725
Nom075 <graytonl@nycstudents.net>
2026-07-30 07:51:27 -0700

    feat(assets): remove menu_level_selected_background.ase

--- res/graphics/png2asset.mk
+++ res/graphics/png2asset.mk
@@ -4,7 +4,6 @@
 PNG2ASSET_ASSETS := \
     backgrounds/level_background \
     backgrounds/menu_background \
-    backgrounds/menu_level_selected_background \
     fillers/black_filler \
     fonts/orochi_jp_16x16 \
     fonts/yarara_font_8x8 \
@@ -18,7 +17,6 @@ PNG2ASSET_ASSETS := \
 # Please keep this list sorted in alphabetical order.
 PNG2ASSET_FLAGS/backgrounds/level_background := -map -use_map_attributes
 PNG2ASSET_FLAGS/backgrounds/menu_background := -map -use_map_attributes
-PNG2ASSET_FLAGS/backgrounds/menu_level_selected_background := -map -use_map_attributes
 PNG2ASSET_FLAGS/fillers/black_filler := -spr8x8 -sw 8 -sh 8
 PNG2ASSET_FLAGS/fonts/orochi_jp_16x16 := -spr8x16 -sw 16 -sh 16
 PNG2ASSET_FLAGS/fonts/yarara_font_8x8 := -spr8x8 -sw 8 -sh 8 -sprite_no_optimize
@@ -26,3 +24,4 @@ PNG2ASSET_FLAGS/notes/a_reverse_note := -spr16x16 -sw 16 -sh 16
 PNG2ASSET_FLAGS/notes/b_reverse_note := -spr16x16 -sw 16 -sh 16
 PNG2ASSET_FLAGS/notes/hold_note := -spr16x16 -sw 16 -sh 16
 PNG2ASSET_FLAGS/notes/tap_note := -spr16x16 -sw 16 -sh 16
+
