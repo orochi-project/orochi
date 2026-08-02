e332fa2b92b760ff93a78ec03ce08e6be22ca472
Nom075 <graytonl@nycstudents.net>
2026-08-02 17:03:06 -0400

    feat(levels): add note struct

--- src/constants/levels.h
+++ src/constants/levels.h
@@ -14,3 +14,14 @@ struct Level {
 
 /** The list of levels in the game. */
 extern const struct Level levels[LEVEL_COUNT];
+
+/** A structure representing each note in a level. */
+typedef struct {
+    uint8_t note_type;          ///<Type of note; includes direction of note.
+    uint8_t grid_x;             ///< The grid X position (0-19).
+    uint8_t grid_y;             ///< The grid Y position (0-17).
+    uint16_t appear_frames;     ///< The frame that the note will appear (0-65535).
+    uint8_t charge_frames;      ///< How much frames to wait before a note should be pressed (0-255).
+    uint8_t hold_frames;        ///< How much frames to hold a note for; applies only for hold notes (0-255).
+} Note;
+
