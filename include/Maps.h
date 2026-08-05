9cff40fbc954dc825f86c3c2072b5a15326ad486
Nom075 <graytonl@nycstudents.net>
2026-08-05 08:32:17 -0700

    refactor: rename level to map

--- include/Maps.h
+++ include/Maps.h
@@ -2,18 +2,18 @@
 
 #include <stdint.h>
 
-/** The number of levels to be defined. */
-#define LEVEL_COUNT 4
+/** The number of maps to be defined. */
+#define MAP_COUNT 4
 
-/** A single level/map. */
+/** A single map. */
 typedef struct {
-    uint8_t id;         ///< The level ID/number.
-    char name[13];      ///< The name of the level (maximum: 12 characters).
-    uint8_t difficulty; ///< The difficulty of the level (1-5).
-} Level;
+    uint8_t id;         ///< The map ID/number.
+    char name[13];      ///< The name of the map (maximum: 12 characters).
+    uint8_t difficulty; ///< The difficulty of the map (1-5).
+} Map;
 
-/** The list of levels in the game. */
-extern const Level levels[LEVEL_COUNT];
+/** The list of maps in the game. */
+extern const Map maps[MAP_COUNT];
 
 /** A note type. */
 typedef enum {
@@ -27,10 +27,10 @@ typedef enum {
               ///< direction of the scanline.
 } NoteType;
 
-/** A note in a level. */
+/** A note in a map. */
 typedef struct {
     NoteType note_type; ///< The type of note.
-    uint8_t grid_idx;   ///< The level grid index to place the note (0-39).
+    uint8_t grid_idx;   ///< The map grid index to place the note (0-39).
     uint16_t
         appear_frame; ///< The frame on which the note will appear (0-65535).
     uint8_t charge_frames; ///< The number of frames to wait before a note
---
+++ src/Maps.c
@@ -0,0 +1,16 @@
+#include "Banks/SetAutoBank.h"
+#include "Levels.h"
+
+const Level maps[MAP_COUNT] = {{
+                                   .name = "Komorebi",
+                                   .difficulty = 1,
+                               },
+                               {
+                                   .name = "Dream Flower",
+                                   .difficulty = 2,
+                               },
+                               {
+                                   .name = "Thirdboot",
+                                   .difficulty = 3,
+                               },
+                               {.name = "Isolation", .difficulty = 4}};
