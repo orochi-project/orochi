9f4b1893c3d3f8ea950e483cf09dbcca43b49dce
v81d <v81d@proton.me>
2026-08-09 12:16:40 -0400

    feat: apply charge frame logic to vertical tap notes

--- src/SpriteTapNoteVertical.c
+++ src/SpriteTapNoteVertical.c
@@ -1,38 +1,49 @@
 #include "Banks/SetAutoBank.h"
 #include "Keys.h"
+#include "Notes.h"
 #include "ZGBMain.h"
 
-#define TIMER_INTERVAL 15
+/** The total number of frames for this note. */
+#define NOTE_FRAME_COUNT 5
 
-typedef struct {
-    uint8_t current_frame;
-    uint8_t timer;
-} CUSTOM_DATA;
+/**
+ * The number of frames after the charge period during which the player
+ * can register a "perfect" click.
+ */
+#define NOTE_PERFECT_FRAMES 8
+
+/**
+ * @def DIV_MUL_ROUND(a, b, c)
+ *
+ * Divide a value after multiplying it, rounding to the nearest integer.
+ *
+ * @param a The value to divide.
+ * @param b The divisor.
+ * @param c The multiplier.
+ */
+#define DIV_MUL_ROUND(a, b, c) (((a) * (c) + (b) / 2) / (b))
 
 void START(void) {
-    CUSTOM_DATA *data = (CUSTOM_DATA *)THIS->custom_data;
-    data->current_frame = 0;
-    data->timer = TIMER_INTERVAL;
+    TapNoteData *note_data = (TapNoteData *)THIS->custom_data;
+    note_data->current_frame = 0;
 }
 
 void UPDATE(void) {
-    CUSTOM_DATA *data = (CUSTOM_DATA *)THIS->custom_data;
+    TapNoteData *note_data = (TapNoteData *)THIS->custom_data;
 
-    --data->timer;
+    ++note_data->current_frame;
 
-    // wait TIMER_INTERVAL until running the next frame animation
-    if (data->timer == 0) {
-        if (data->current_frame < 4)
-            ++data->current_frame;
-        else
-            data->current_frame = 0;
-
-        data->timer = TIMER_INTERVAL;
-        SetFrame(THIS, data->current_frame);
+    // charge period
+    if (note_data->current_frame <= note_data->charge_frames) {
+        uint16_t note_frame =
+            DIV_MUL_ROUND(note_data->current_frame, note_data->charge_frames,
+                          NOTE_FRAME_COUNT - 1);
+        SetFrame(THIS, note_frame);
     }
 
     // If the note collides with the scanline and the correct direction is
-    // pressed, delete the note
+    // pressed, delete the note.
+    // TODO: check for early hits
     if (CheckCollision(THIS, scanline_sprite)) {
         if ((THIS->mirror && KEY_TICKED(J_DOWN)) ||
             (!THIS->mirror && KEY_TICKED(J_UP))) {
@@ -40,6 +51,15 @@ void UPDATE(void) {
             return;
         }
     }
+
+    // perfect period
+    if (note_data->current_frame >=
+        note_data->charge_frames + NOTE_PERFECT_FRAMES) {
+        SpriteManagerRemoveSprite(THIS);
+        return;
+    }
+
+    // TODO: check for misses
 }
 
 void DESTROY(void) {}
