5385388c4b77c55d169168cd40f7ef157c0a8187
v81d <v81d@proton.me>
2026-08-09 22:38:42 -0400

    refactor: anchor scanline on top-left instead of center

--- include/Scanline.h
+++ include/Scanline.h
@@ -3,14 +3,14 @@
 #include "Sprite.h"
 
 /** The starting x-position of the scanline. */
-#define SCANLINE_START_X 8
+#define SCANLINE_START_X 4
 /** The starting y-position of the scanline. */
-#define SCANLINE_START_Y 64
+#define SCANLINE_START_Y 8
 
 /** The x-position of the left boundary of the scanline. */
-#define SCANLINE_BOUND_LEFT_X 9
+#define SCANLINE_BOUND_LEFT_X 5
 /** The x-position of the right boundary of the scanline. */
-#define SCANLINE_BOUND_RIGHT_X 151
+#define SCANLINE_BOUND_RIGHT_X 147
 
 /** The starting velocity of the scanline. */
 #define SCANLINE_START_VELOCITY 1
